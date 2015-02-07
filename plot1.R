21fb17ef84b517cbbc082dee17523aa5c3d5ae4d
Debashis <fireonthewater07@gmail.com>
2015-02-07 12:57:33 +0100

    2nd commit Course project 1

--- plot1.R
+++ plot1.R
@@ -1,10 +1,12 @@
-data<-read.table(file = "household_power_consumption.txt", header=TRUE,sep = ";", na.strings = "?",nrows=2075259)
+##Read in the data with read.table. One can also use fread from data.table package which is faster but for this problem read.table is good enough.
+data<-read.table(file = "household_power_consumption.txt", header=TRUE, sep = ";", na.strings = "?",nrows=2075259, stringsAsFactors = FALSE)
+
+# Convert the 1st column to objects of Date class.
 data[,1]<-as.Date(data$Date, format = "%d/%m/%Y")
+
+#Extract the data for the required two days only.
 data_sub<-subset(data,data$Date=="2007-02-01"|data$Date=="2007-02-02")
 
-#data<-read.table(file = "household_power_consumption.txt", header=FALSE,sep = ";", na.strings = "?",nrows=2880,skip=66636)
-#with(dat, hist(data$Global_active_power ,main = "Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "Frequency", col="red"))
-png("plot1.png",width = 480, height = 480)
+png("plot1.png",width = 480, height = 480)  #opens the graphics device (png)
 with(data_sub, hist(data_sub$Global_active_power ,main = "Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "Frequency", col="red"))
-#with(data, hist(data[[3]] ,main = "Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "Frequency", col="red"))
-dev.off()
+dev.off()    # Closes the graphics device
